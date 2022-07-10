83d9b50c2b5c75fc69f8a821498b635d3c074682
Tran-Kevin-Dev <roy_kevin_tran@protonmail.com>
2022-07-10 16:16:15 +0200

    bootstrap LG + script.js apart gezet

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-// const tween = KUTE.fromTO(
-//   "#blobA1",
-//   { path: "#blobA1" },
-//   { path: "#blobA2" },
-//   { repeat: 999, duration: 300, yoyo: true }
-// ).start();
+const tween = KUTE.fromTo(
+  "#blobA1",
+  { path: "#blobA1" },
+  { path: "#blobA2" },
+  { repeat: 999, duration: 2000, yoyo: true }
+).start();
